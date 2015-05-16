5bfbaee53797ef1d5b9e0d0d5418c7d8b4b3d85b
Jim Fingal <jim.fingal@gmail.com>
2015-05-16 14:22:09 -0700

    More nuanced monkey patching

--- bot/monkey.py
+++ bot/monkey.py
@@ -1,11 +1,17 @@
 import images2gif
+from PIL.GifImagePlugin import getheader
 
 
 def patch_image_headers():
     """Monkey-patches an enclosed function within images2gif.writeGif."""
 
     def patched_get_header(im):
-        ret = im.palette.getdata()
-        return ret
+
+        header = getheader(im)
+
+        if header[1] is not None:
+            return header
+        else:
+            return im.palette.getdata()
 
     images2gif.writeGif.__globals__['getheader'] = patched_get_header
